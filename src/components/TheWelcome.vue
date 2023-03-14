7b79f01af3f73c4f379fd54b3b7a3e7708e66aad
salsabilaathika-97 <salsathikar@gmail.com>
2023-03-14 13:19:47 +0800

    create get all data from api

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,7 +1,25 @@
 <script>
-
+  export default {
+    data() {
+      return {
+        listItems: []
+      }
+    },
+    methods: {
+      async getData() {
+        const res = await fetch("https://fakestoreapi.com/products")
+        const finalRes = await res.json();
+        this.listItems = finalRes;
+      }
+    },
+    mounted() {
+      this.getData()
+    }
+  }
 </script>
 
 <template>
-  <h1>Hello</h1>
+  <div v-for = "item in listItems" :key="item">
+    {{ item.title }}
+  </div>
 </template>
